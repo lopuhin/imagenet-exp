6cbf86bfb0578eafc0333f602d4f5482d90a45a5
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2019-02-09 10:47:40 +0300

    Small style fixes

--- imagenet/main.py
+++ imagenet/main.py
@@ -1,8 +1,8 @@
 import argparse
-import os
 import random
 import shutil
 import time
+from pathlib import Path
 import warnings
 
 import torch
@@ -10,59 +10,49 @@ import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torch.optim
-import torch.utils.data
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
 
 
-model_names = sorted(name for name in models.__dict__
-    if name.islower() and not name.startswith("__")
-    and callable(models.__dict__[name]))
-
-parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-parser.add_argument('data', metavar='DIR',
-                    help='path to dataset')
-parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
-                    choices=model_names,
-                    help='model architecture: ' +
-                        ' | '.join(model_names) +
-                        ' (default: resnet18)')
-parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                    help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=256, type=int,
-                    metavar='N',
-                    help='mini-batch size (default: 256), this is the total '
-                         'batch size of all GPUs on the current node when '
-                         'using Data Parallel')
-parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
-                    metavar='LR', help='initial learning rate', dest='lr')
-parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                    help='momentum')
-parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
-                    metavar='W', help='weight decay (default: 1e-4)',
-                    dest='weight_decay')
-parser.add_argument('-p', '--print-freq', default=10, type=int,
-                    metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
-                    help='evaluate model on validation set')
-parser.add_argument('--pretrained', dest='pretrained', action='store_true',
-                    help='use pre-trained model')
-parser.add_argument('--seed', default=None, type=int,
-                    help='seed for initializing training. ')
-parser.add_argument('--gpu', default=None, type=int,
-                    help='GPU id to use.')
-
-best_acc1 = 0
-
-
 def main():
+    model_names = sorted(name for name in models.__dict__
+                         if name.islower() and not name.startswith("__")
+                         and callable(models.__dict__[name]))
+
+    parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+    arg = parser.add_argument
+    arg('data', help='path to dataset')
+    arg('--arch', default='resnet18',
+        choices=model_names,
+        help='model architecture: ' +
+             ' | '.join(model_names) +
+             ' (default: %(default)s)')
+    arg('--workers', default=4, type=int,
+        help='number of data loading workers (default: %(default)s)')
+    arg('--epochs', default=90, type=int,
+        help='number of total epochs to run')
+    arg('--start-epoch', default=0, type=int,
+        help='manual epoch number (useful on restarts)')
+    arg('--batch-size', default=256, type=int,
+        help='mini-batch size (default: %(default)s), this is the total '
+             'batch size of all GPUs on the current node when '
+             'using Data Parallel')
+    arg('--lr', default=0.1, type=float,
+        help='initial learning rate')
+    arg('--momentum', default=0.9, type=float, help='momentum')
+    arg('--weight-decay', default=1e-4, type=float,
+        help='weight decay (default: %(default)s)')
+    arg('--print-freq', default=10, type=int,
+        help='print frequency (default: %(default)s)')
+    arg('--resume', default='', type=str,
+        help='path to latest checkpoint (default: %(default)s)')
+    arg('--evaluate', dest='evaluate', action='store_true',
+        help='evaluate model on validation set')
+    arg('--pretrained', action='store_true', help='use pre-trained model')
+    arg('--seed', default=None, type=int, help='seed for initializing training')
+    arg('--gpu', default=None, type=int, help='GPU id to use')
     args = parser.parse_args()
 
     if args.seed is not None:
@@ -79,23 +69,12 @@ def main():
         warnings.warn('You have chosen a specific GPU. This will completely '
                       'disable data parallelism.')
 
-    gpu = args.gpu
-
-    global best_acc1
-    args.gpu = gpu
-
-    if args.gpu is not None:
-        print("Use GPU: {} for training".format(args.gpu))
-
-    # create model
-    if args.pretrained:
-        print("=> using pre-trained model '{}'".format(args.arch))
-        model = models.__dict__[args.arch](pretrained=True)
-    else:
-        print("=> creating model '{}'".format(args.arch))
-        model = models.__dict__[args.arch]()
+    print(f'Using model {args.arch} pretrained={args.pretrained}')
+    model_cls = models.__dict__[args.arch]
+    model = model_cls(pretrained=args.pretrained)
 
     if args.gpu is not None:
+        print('Use GPU: {args.gpu} for training')
         torch.cuda.set_device(args.gpu)
         model = model.cuda(args.gpu)
     else:
@@ -106,32 +85,29 @@ def main():
         else:
             model = torch.nn.DataParallel(model).cuda()
 
-    # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda(args.gpu)
-
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
 
-    # optionally resume from a checkpoint
+    best_acc1 = 0
+
     if args.resume:
-        if os.path.isfile(args.resume):
-            print("=> loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_acc1 = checkpoint['best_acc1']
-            model.load_state_dict(checkpoint['state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer'])
-            print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.resume, checkpoint['epoch']))
-        else:
-            print("=> no checkpoint found at '{}'".format(args.resume))
+        if not Path(args.resume).is_file():
+            parser.error(f'checkpoint {args.resume} is not a file')
+        print(f'loading checkpoint {args.resume}')
+        checkpoint = torch.load(args.resume)
+        args.start_epoch = checkpoint['epoch']
+        best_acc1 = checkpoint['best_acc1']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        print(f'loaded checkpoint {args.resume} (epoch {checkpoint["epoch"]})')
 
     cudnn.benchmark = True
 
-    # Data loading code
-    traindir = os.path.join(args.data, 'train')
-    valdir = os.path.join(args.data, 'val')
+    data_root = Path(args.data)
+    traindir = data_root / 'train'
+    valdir = data_root / 'val'
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
@@ -144,19 +120,26 @@ def main():
             normalize,
         ]))
 
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=args.batch_size, shuffle=True,
-        num_workers=args.workers, pin_memory=True)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.workers,
+        pin_memory=True,
+    )
 
-    val_loader = torch.utils.data.DataLoader(
+    val_loader = DataLoader(
         datasets.ImageFolder(valdir, transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             normalize,
         ])),
-        batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.workers,
+        pin_memory=True,
+    )
 
     if args.evaluate:
         validate(val_loader, model, criterion, args)
@@ -184,14 +167,14 @@ def main():
         }, is_best)
 
 
-def train(train_loader, model, criterion, optimizer, epoch, args):
+def train(train_loader: DataLoader, model: nn.Module, criterion, optimizer,
+          epoch, args):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
     top5 = AverageMeter()
 
-    # switch to train mode
     model.train()
 
     end = time.time()
@@ -223,14 +206,13 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         end = time.time()
 
         if i % args.print_freq == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                   epoch, i, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+            print(f'Epoch: [{epoch}][{i}/{len(train_loader)}]',
+                  f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})',
+                  f'Data {data_time.val:.3f} ({data_time.avg:.3f})',
+                  f'Loss {loss.val:.4f} ({loss.avg:.4f})',
+                  f'Acc@1 {top1.val:.3f} ({top1.avg:.3f})',
+                  f'Acc@5 {top5.val:.3f} ({top5.avg:.3f})',
+                  sep='\t')
 
 
 def validate(val_loader, model, criterion, args):
@@ -239,7 +221,6 @@ def validate(val_loader, model, criterion, args):
     top1 = AverageMeter()
     top5 = AverageMeter()
 
-    # switch to evaluate mode
     model.eval()
 
     with torch.no_grad():
@@ -264,24 +245,22 @@ def validate(val_loader, model, criterion, args):
             end = time.time()
 
             if i % args.print_freq == 0:
-                print('Test: [{0}/{1}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                       i, len(val_loader), batch_time=batch_time, loss=losses,
-                       top1=top1, top5=top5))
+                print(f'Test: [{i}/{len(val_loader)}]',
+                      f'Time {batch_time.val:.3f} ({batch_time.avg:.3f})',
+                      f'Loss {loss.val:.4f} ({loss.avg:.4f})',
+                      f'Acc@1 {top1.val:.3f} ({top1.avg:.3f})',
+                      f'Acc@5 {top5.val:.3f} ({top5.avg:.3f})',
+                      sep='\t')
 
-        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-              .format(top1=top1, top5=top5))
+        print(f' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}')
 
     return top1.avg
 
 
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+def save_checkpoint(state, is_best, filename='checkpoint.pth'):
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
+        shutil.copyfile(filename, 'model_best.pth')
 
 
 class AverageMeter(object):
